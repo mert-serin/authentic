042007e6175af6f7f09e9428635503e279d87749
Mert Serin <mert.serin@barakatech.com>
2018-10-26 17:32:10 +0300

    URLConstants class has been added

---
+++ AuthenticCompany/Helpers/URLConstants.swift
@@ -0,0 +1,33 @@
+//
+//  URLConstants.swift
+//  AuthenticCompany
+//
+//  Created by Mert Serin on 26.10.2018.
+//  Copyright © 2018 Mert Serin. All rights reserved.
+//
+
+import Foundation
+class URLConstants {
+    
+    // TODO: NEVER FORGET TO MODIFY HERE! (do NOT remove this comment line ever!)
+    internal static let environment: Environments = Environments.Live
+    
+    private static let _server = URLConstants.getURL()
+    private static let _endPoint = "\(_server)"
+    
+    
+    private static func getURL() -> String {
+        
+        switch environment {
+        case .Local:
+            return ""
+            
+        case .Test:
+            return ""
+            
+        case .Live:
+            return ""
+        }
+    }
+    
+}
