07271296f11528c301847679ba3fea58016cef6b
Mert Serin <mert.serin@barakatech.com>
2018-10-26 17:14:34 +0300

    Initial Commit

---
+++ AuthenticCompany/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AuthenticCompany
+//
+//  Created by Mert Serin on 26.10.2018.
+//  Copyright © 2018 Mert Serin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
